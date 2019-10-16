e06c79f70106ca4564f9cc100df69a56b2f49aa2
Guilherme Caires <pessoalgcaires@gmail.com>
2019-10-16 03:46:43 -0300

    Refatoração do css

---
+++ View/post.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="https://www.instagram.com/static/images/ico/favicon.ico/36b3ee2d91ed.ico" type="image/x-icon"/>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/aux.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/all.js"></script>
+    <title>InstaMVP - <?php echo (user = oi)?></title>
+</head>
+<body>
+    <div class="content">
+        
+    </div>
+
+</body>
+</html>
