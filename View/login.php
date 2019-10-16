1c9383e26c5dbf74ddfd2a29b051cd6ffa2d6ce9
Guilherme <pessoalgcaires@gmail.com>
2019-10-16 11:24:18 -0300

    Banco de dados criado

--- View/login.php
+++ View/login.php
@@ -1,31 +1,25 @@
 <?php
-             include_once ("/wamp64/www/instaMVP/Controller/Conexao.php");
-             session_start ();
-               
-              if( isset($_POST['entrar'])){
-                $erros = array();
-                $phoneMail = mysqli_real_scape_string($conexao, $_POST['phoneMail']);
-                $senha = mysqli_real_scape_string($conexao, $_POST['senha']);
+  include_once ("./Controller/Conexao.php");
+  session_start ();
+    
+  if( isset($_POST['entrar'])){
+    $erros = array();
+    $phoneMail = mysqli_real_scape_string($conexao, $_POST['phoneMail']);
+    $password = mysqli_real_scape_string($conexao, $_POST['password']);
 
-                if(empty($phoneMail) or empty($senha)){
-                 $erros[] = "<li> O campo login e senha precisam ser preenchidos</li>";
-                 
-                }
-                else{
-                 $sql = "SELECT phoneMail FROM usuarios WHERE phoneMail = '$phoneMail'";
-                 $resultado = mysqli_query($conexao,$sql);
-                 
+    if(empty($phoneMail) or empty($password)){
+      $erros[] = "<li> O campo login e password precisam ser preenchidos</li>";
+    }else{
+      $sql = "SELECT phoneMail FROM usuarios WHERE phoneMail = '$phoneMail'";
+      $resultado = mysqli_query($conexao,$sql);
+    }
 
-                 }
-                 if(mysqli_num_rows($resultado) > 0){
-                     $senha = ($senha);
-                     $sql = "SELECT * FROM usuarios WHERE phoneMail = '$phoneMail' AND senha = '$senha'";
-                     echo "<p style= 'color: green; font-size: 40px;'>LOGADO</p>";   
-                    
-
-                     $resultado = mysqli_query($conexao,$sql);
-       mysqli_close($conexao);
-                   
-                 }
-            }
-            ?>
+    if(mysqli_num_rows($resultado) > 0){
+      $password = ($password);
+      $sql = "SELECT * FROM usuarios WHERE phoneMail = '$phoneMail' AND password = '$password'";
+      echo "<p style= 'color: green; font-size: 40px;'>LOGADO</p>";   
+      $resultado = mysqli_query($conexao,$sql);
+      mysqli_close($conexao);
+    }
+  }
+?>
---
+++ instamvp.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 16-Out-2019 às 16:23
+-- Versão do servidor: 10.1.38-MariaDB
+-- versão do PHP: 7.3.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `instamvp`
+--
+CREATE DATABASE IF NOT EXISTS `instamvp` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `instamvp`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idUser` int(11) NOT NULL,
+  `photo` varchar(40) NOT NULL,
+  `subtitle` varchar(300) NOT NULL,
+  `hashtags` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phoneMail` varchar(40) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `user` varchar(20) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `bio` text,
+  `verified` tinyint(1) DEFAULT NULL,
+  `followers` text,
+  `followed` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phoneMail` (`phoneMail`),
+  UNIQUE KEY `user` (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
